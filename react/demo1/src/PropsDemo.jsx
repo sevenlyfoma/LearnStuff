8c2368c0890aee3b517e44d30b5abd13a1eb69e2
7lyfoma <7lyfoma@gmail.com>
2026-02-02 13:36:12 +0000

    Started on a react tutorial of properties

--- react/demo1/src/PropsDemo.jsx
+++ react/demo1/src/PropsDemo.jsx
@@ -1,14 +1,19 @@
+//https://www.w3schools.com/REACT/react_props_destructuring.asp
+
 function Car(props) {
   return (
-    <h2>I am a {props.brand}!</h2>
+    <h2>I am a {props.color} {props.brand} {props.model}, with {props.carinfo.fueltype} fuel and {props.carinfo.painttype} paint in year {props.year} </h2>
   );
 }
 
 
 function PropsDemo() {
+    let x = "Ford";
+    let y = {fueltype: "leaded", painttype:"also leaded"}
+
     return (
             <div>
-                <Car brand="Ford" />
+                <Car brand={x} model="Mustang" color="red" year={1969} carinfo={y}/>
             </div>
         )
 }
