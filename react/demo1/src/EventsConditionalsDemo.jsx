e375b05104f21fd9219e87cb59248d933e4645a2
7lyfoma <7lyfoma@gmail.com>
2026-02-04 11:28:56 +0000

    continued with react tutorial

--- react/demo1/src/EventsConditionalsDemo.jsx
+++ react/demo1/src/EventsConditionalsDemo.jsx
@@ -1,9 +1,18 @@
 //https://www.w3schools.com/REACT/react_events.asp
+function Football() {
+  const shoot = () => {
+    alert("Great Shot!");
+  }
 
+  return (
+    <button onClick={shoot}>Take the shot!</button>
+  );
+}
 
 function EventsConditionalsDemo() {
      return (
             <div>
+                <Football />
             </div>
         )
 }
