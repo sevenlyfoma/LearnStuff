bafc5a012d1345930b5d36f60e52e5db6afd4c58
7lyfoma <7lyfoma@gmail.com>
2026-02-05 14:07:09 +0000

    continued with react tutorial,  added conditional and events stuff

--- react/demo1/src/EventsConditionalsDemo.jsx
+++ react/demo1/src/EventsConditionalsDemo.jsx
@@ -1,11 +1,65 @@
 //https://www.w3schools.com/REACT/react_events.asp
+
+
+function MissedGoal() {
+  return <h2>MISSED!</h2>;
+}
+
+function MadeGoal() {
+  return <h2>Goal!</h2>;
+}
+
+function Goal(props) {
+  const isGoal = props.isGoal;
+  // if (isGoal) { This is the same as the below ternary operator
+  //   return <MadeGoal/>;
+  // }
+  // return <MissedGoal/>;
+
+  return (
+    <>
+      { isGoal ? <MadeGoal/> : <MissedGoal/> }
+    </>
+  );
+}
+
+function ConditionalCar(props) {
+  return (
+    <>
+      {props.brand && <h2>My car is a {props.brand}!</h2>}
+    </>
+  );
+}
+
 function Football() {
   const shoot = () => {
     alert("Great Shot!");
   }
+  const shoot2 = (a) => {
+    alert(a);
+  }
+  const shoot3 = (a, b) => {
+    alert(b.type);
+    /*
+    'b' represents the React event that triggered the function,
+    in this case the 'click' event
+    */
+  }
 
   return (
-    <button onClick={shoot}>Take the shot!</button>
+    <div>
+      <button onClick={shoot}>Take the shot!</button>
+      <button onClick={() => shoot2("Goal!")}>Take the shot!</button>
+      <button onClick={(event) => shoot3("Goal!", event)}>Take the shot!</button>
+
+      <Goal isGoal={false} />
+      <Goal isGoal={true} />
+
+      <ConditionalCar brand="Ford" />
+      <ConditionalCar/>
+
+    </div>
+    
   );
 }
 
